bc6b04c09904782b9924f324da69cc7870017acd
Natalia Tepluhina <tarya.se@gmail.com>
2019-06-24 22:34:31 +0300

    Added d3 and created an abstract component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <Chart />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Chart from './components/Chart'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Chart
   }
 }
 </script>
---
+++ src/components/Chart.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    I AM CHART
+  </div>
+</template>

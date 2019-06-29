a5bfb943afd355afc01a974db0ee0f6e4544937f
Natalia Tepluhina <tarya.se@gmail.com>
2019-06-29 14:34:40 +0300

    Fixed an example to draw packed stars

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,79 +1,99 @@
 <template>
   <div class="wrapper">
-    <svg width="500" height="500">
-      <defs>
-        <radialGradient id="exampleGradient">
-          <stop offset="2%" stop-color="white" />
-          <stop offset="95%" :stop-color="starColor" />
-        </radialGradient>
-      </defs>
+    <svg width="800" height="800">
       <rect width="100%" height="100%" fill="#0E0432" />
-      <path
-        class="radial"
-        :d="radialData"
-        transform="translate(250, 250)"
-        fill="url(#exampleGradient)"
-      ></path>
+      <g
+        v-for="repo in layoutData.children"
+        :key="repo.data.name"
+        :style="{
+          transform: `translate(${repo.x}px, ${repo.y}px)`,
+        }"
+      >
+        <defs>
+          <radialGradient :id="`starGradient-${repo.data.name}`">
+            <stop offset="2%" stop-color="white" />
+            <stop offset="95%" :stop-color="starColor(repo.data)" />
+          </radialGradient>
+        </defs>
+        -->
+        <path
+          class="radial"
+          :d="radialData(repo)"
+          :fill="`url(#starGradient-${repo.data.name})`"
+        ></path>
+      </g>
     </svg>
-    <aside>
-      <div class="range-input">
-        <input type="range" name="rays" min="4" max="60" v-model="rays" />
-        <label for="rays">Rays</label>
-      </div>
-      <div class="range-input">
-        <input type="range" name="radius" min="10" max="50" v-model="radius" />
-        <label for="radius">Radius</label>
-      </div>
-      <div class="range-input">
-        <input type="range" name="heat" min="1" max="100" v-model="heat" />
-        <label for="rays">Heat</label>
-      </div>
-    </aside>
   </div>
 </template>
 
 <script>
-import { interpolateCubehelixDefault } from 'd3';
 import { scaleLinear, scaleSequential } from 'd3-scale';
 import { lineRadial } from 'd3-shape';
+import { min, max } from 'd3-array';
+import { hierarchy, pack } from 'd3-hierarchy';
+import dataset from '../assets/dataset';
 export default {
   data() {
     return {
-      radius: 20,
-      rays: 5,
-      heat: 5,
+      repositories: dataset,
     };
   },
-  computed: {
-    outerCircleRadius() {
+  methods: {
+    minValue(prop) {
+      const arr = this.repositories.map(repo => repo[prop]);
+      return min(arr);
+    },
+    maxValue(prop) {
+      const arr = this.repositories.map(repo => repo[prop]);
+      return max(arr);
+    },
+    radialPoints(repo) {
       const scale = scaleLinear()
-        .domain([0, 100])
-        .range([0, 300]);
-      return scale(this.radius);
-    },
-    innerCircleRadius() {
-      return this.outerCircleRadius * 0.5;
-    },
-    radialPoints() {
-      const step = 2 / (this.rays * 2);
+        .domain([this.minValue('contributors'), this.maxValue('contributors')])
+        .range([4, 80]);
+      const rays = Math.round(scale(repo.data.contributors));
+      const step = 2 / (rays * 2);
       const points = [];
       for (let i = 0; i <= 2; i += step) {
-        const interval = Math.round(i * this.rays);
-        const radius =
-          interval % 2 ? this.innerCircleRadius : this.outerCircleRadius;
+        const interval = Math.round(i * rays);
+        const radius = interval % 2 ? repo.r * 0.5 : repo.r;
         points.push([Math.PI * i, radius]);
       }
       return points;
     },
-    radialData() {
+    radialData(repo) {
       const radialLineGenerator = lineRadial();
-      return radialLineGenerator(this.radialPoints);
+      return radialLineGenerator(this.radialPoints(repo));
     },
-    starColor() {
+    starColor(repo) {
+      const scaledIssues = scaleLinear()
+        .domain([this.minValue('issues'), this.maxValue('issues')])
+        .range([0, 100]);
       const myColor = scaleLinear()
         .domain([0, 25, 50, 75, 100])
         .range(['#ff7665', '#ffb469', '#ffe876', '#fff', '#99cdff']);
-      return myColor(this.heat);
+      return myColor(scaledIssues(repo.issues));
+    },
+  },
+  computed: {
+    transformedRepoData() {
+      return {
+        name: 'Top Level',
+        children: this.repositories.map(repo => ({
+          ...repo,
+          parent: 'Top Level',
+        })),
+      };
+    },
+    layoutData() {
+      const rootHierarchy = hierarchy(this.transformedRepoData)
+        .sum(d => d.stars)
+        .sort((a, b) => {
+          return b.value - a.value;
+        });
+      return pack()
+        .size([800, 800])
+        .padding(40)(rootHierarchy);
     },
   },
 };
